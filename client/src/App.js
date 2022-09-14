975413d31a58aab8cb868b02b8bae48137255a66
Kaleb Franken <skywrithin@gmail.com>
2022-09-14 10:03:43 -0600

    all dependencies installed correctly and boilerplate done

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,14 @@
 import { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import './App.css';
-
 import Main from './components/Main';
 
-export function App() {
+export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Main/>} >
-        
+
         </Route>
       </Routes>
     </Router>
--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,16 +1,12 @@
 //Main is the wrapper page for all pages
-import logo from 'client/src/logo.svg';
+import logo from '../logo.svg';
 
-
-export function Main() {
-  return (
-      <div className="App">
-          <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <p>
-                  Edit <code>src/App.js</code> and save to reload.
-              </p>
-          </header>
-      </div>
-  );
+export default function Main() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+            </header>
+        </div>
+    );
 }
