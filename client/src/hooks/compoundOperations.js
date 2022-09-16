271e48264a137098f59621a8f5f7fe193c45ef2f
Kaleb Franken <skywrithin@gmail.com>
2022-09-15 20:23:56 -0600

    fixed compound operations

--- client/src/hooks/compoundOperations.js
+++ client/src/hooks/compoundOperations.js
@@ -4,7 +4,6 @@ const lowercaseSigma0 = (x) => {
     let a = rotr(x, 7);
     let b = rotr(x, 18);
     let c = x >> 3;
-
     let j = xOR(xOR(a, b), c);
     return j
 };
@@ -13,7 +12,6 @@ const lowercaseSigma1 = (x) => {
     let a = rotr(x, 17);
     let b = rotr(x, 19);
     let c = x >> 10;
-
     let j = xOR(xOR(a, b), c);
     return j;
 }
@@ -22,7 +20,6 @@ const uppercaseSigma0 = (x) => {
     let a = rotr(x, 2);
     let b = rotr(x, 13);
     let c = rotr(x, 22)
-
     let j = xOR(xOR(a, b), c);
     return j;
 };
