83c479e6f7a2022348d15005b538b78d8de9b4f4
Kaleb Franken <skywrithin@gmail.com>
2022-09-24 20:02:16 -0600

    new context hook

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,31 @@
 //React Imports
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Main from './components/Main';
 import Message from './components/pages/Message';
 import Hashing from './components/pages/Hashing';
 
+
 const path1 = '';
-const path2 = 'hashing';
 
 export default function App() {
     const [initMsg, setInitMsg] = useState([]);
+    const [clippyStr, setClippyStr] = useState("")
+    const { context } = React.createContext(clippyStr);
 
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Main />}>
-                    <Route
-                        path={`/${path1}`}
-                        element={<Message setInitMsg={setInitMsg} />}
-                    />
-                </Route>
-            </Routes>
-        </Router>
+        <context.Provider value={clippyStr}>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Main />}>
+                        <Route
+                            path={`/${path1}`}
+                            element={<Message setInitMsg={setInitMsg} />}
+                        />
+                    </Route>
+                </Routes>
+            </Router>
+        </context.Provider>
     );
 }
