c1d0e8869f732591143391da63d7b17748361f49
Kaleb Franken <kaleb.franken7@gmail.com>
2022-10-19 12:32:09 -0600

    fixed spacing

--- client/src/components/cComponents/BinaryDisplay.js
+++ client/src/components/cComponents/BinaryDisplay.js
@@ -3,6 +3,6 @@ import { intTo32BitStr } from '../../hooks/converters';
 export default function BinaryDisplay({ integer }) {
     if (typeof integer === "number") {
         const binaryString = intTo32BitStr(integer);
-        return <div className="w-fit h-3 w-full  ml-2">{binaryString}</div>;
+        return <div className="w-fit h-[13.5px] w-full  ml-2">{binaryString}</div>;
     }
 }
