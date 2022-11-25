88f24200cf2b72700c669cc8618ca734be8971bd
Kaleb Franken <kaleb.franken7@gmail.com>
2022-11-25 12:46:47 -0700

    cleanup

--- client/src/components/pages/Message.js
+++ client/src/components/pages/Message.js
@@ -7,82 +7,91 @@ import { anyToBinaryStr, intTo64BinaryStrArr } from '../../hooks/converters';
 export default function FirstPage() {
     const [Input] = useOutletContext();
     const [initMsg, setInitMsg] = useState();
-    const [bytesArr, setBytesArr] = useState([]);
+    const [bytesArr, setBytesArr] = useState(Input.split(""));
     const [binaryDisplay, setBinaryDisplay] = useState([]);
 
     const [DCS, setDCS] = useState({
         first: true,
-        seccond: false,
+        seccond: true,
         third: false,
         fourth: false,
     });
 
 
     useEffect(() => {
-        setBytesArr([...Input].map((c) => c.charCodeAt(0).toString() + ' '));
-        setTimeout(() => {
-            setDCS((DCS) => ({ ...DCS, seccond: true }));
-        }, 1000);
-    }, []);
+        console.log(bytesArr)
 
-    useEffect(() => {
-        //Binary Display Control//
-        setTimeout(() => {
-            setDCS((DCS) => ({ ...DCS, third: true }));
-            let bArr = [...bytesArr].map((byte) => {
-                return anyToBinaryStr(byte);
-            });
-            for (let i = 0; i <= bArr.length - 1; i++) {
-                setTimeout(() => {
-                    setBinaryDisplay(bArr.slice(0, i + 1));
-                }, 300 * i);
-                if (i === bArr.length - 1) {
-                    setTimeout(() => {
-                        setBinaryDisplay((prevState) => {
-                            if (typeof prevState === 'string') return;
-                            return prevState ? prevState.join('') : 'empty';
-                        });
-                    }, 400 * bArr.length);
-                }
-            }
+        setTimeout(()=>{
+            for(let i=0; )
+        }, )
+        // setBytesArr([...Input].map((c) => c.charCodeAt(0).toString() + ' '));
 
-            setTimeout(() => {
-                setBinaryDisplay((prevState) => {
-                    if (typeof prevState === 'string') return prevState + '1';
-                });
-            }, 400 * (bArr.length + 1));
+        // setTimeout(() => {
 
-            setTimeout(() => {
-                setBinaryDisplay((prevState) => {
-                    let padL = 446 - (prevState.length - 1);
-                    if (typeof prevState === 'string') {
-                        let padding = '';
-                        for (let i = 0; i <= padL; i++) {
-                            padding = padding + '0';
-                        }
-                        setInitMsg(
-                            prevState +
-                            padding +
-                            intTo64BinaryStrArr(padL).join('')
-                        );
-                        return (
-                            prevState +
-                            padding +
-                            intTo64BinaryStrArr(padL).join('')
-                        );
-                    }
-                    return prevState;
-                });
-            }, 400 * (bArr.length + 2));
-        }, 1500);
+        //     setDCS((DCS) => ({ ...DCS, seccond: true }));
+        // }, 1000);
     }, [bytesArr]);
 
-    useEffect(() => {
-        if (typeof initMsg === !"string") return null;
-        if (initMsg){
-            setDCS((DCS) => ({ ...DCS, fourth: true }));
-        }
-    }, [initMsg]);
+    // useEffect(() => {
+    //     //Binary Display Control//
+    //     setTimeout(() => {
+    //         setDCS((DCS) => ({ ...DCS, third: true }));
+    //         let bArr = [...bytesArr].map((byte) => {
+    //             return anyToBinaryStr(byte);
+    //         });
+    //         for (let i = 0; i <= bArr.length - 1; i++) {
+
+    //             setTimeout(() => {
+    //                 setBinaryDisplay(bArr.slice(0, i + 1));
+    //             }, 300 * i);
+
+    //             if (i === bArr.length - 1) {
+    //                 setTimeout(() => {
+    //                     setBinaryDisplay((prevState) => {
+    //                         if (typeof prevState === 'string') return;
+    //                         return prevState ? prevState.join('') : 'empty';
+    //                     });
+    //                 }, (400 * bArr.length) + 500 );
+    //             }
+    //         }
+
+    //         setTimeout(() => {
+    //             setBinaryDisplay((prevState) => {
+    //                 if (typeof prevState === 'string') return prevState + '1';
+    //             });
+    //         }, 400 * (bArr.length + 1) + 50);
+
+    //         setTimeout(() => {
+    //             setBinaryDisplay((prevState) => {
+    //                 let padL = 446 - (prevState.length - 1);
+    //                 if (typeof prevState === 'string') {
+    //                     let padding = '';
+    //                     for (let i = 0; i <= padL; i++) {
+    //                         padding = padding + '0';
+    //                     }
+    //                     setInitMsg(
+    //                         prevState +
+    //                         padding +
+    //                         intTo64BinaryStrArr(padL).join('')
+    //                     );
+    //                     return (
+    //                         prevState +
+    //                         padding +
+    //                         intTo64BinaryStrArr(padL).join('')
+    //                     );
+    //                 }
+    //                 return prevState;
+    //             });
+    //         }, 400 * (bArr.length + 2));
+    //     }, 1500);
+    // }, [bytesArr]);
+
+    // useEffect(() => {
+    //     if (typeof initMsg === !"string") return null;
+    //     if (initMsg){
+    //         setDCS((DCS) => ({ ...DCS, fourth: true }));
+    //     }
+    // }, [initMsg]);
 
     return (
         <div className="outline outline-3 outline-black rounded h-full w-82 p-2 bg-black text-green-400">
